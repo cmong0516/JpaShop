9b8e205f8825267d675f2b6561d6c7dd48fcb60c
cmong0516 <cmong0516@gmail.com>
2022-08-17 22:32:00 +0900

    2022.08.17

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -27,5 +27,27 @@ class MemberServiceTest {
         Assertions.assertEquals(member, memberRepository.findOne(saveId));
     }
 
+    @Test
+    public void validationTest() throws Exception{
+        Member member1 = new Member();
+        member1.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+
+//        memberService.join(member1);
+//        memberService.join(member2);
+//
+//        // 예외 터져야함
+//        // 터짐 :) java.lang.IllegalStateException: 이미 존재하는 회원입니다.
+
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
+        System.out.println("이거 나오면 곤란.");
+    }
+
 
 }
