561b3f516a07f7ca812f133fee19898d037b00dc
cmong0516 <cmong0516@gmail.com>
2022-08-24 12:47:54 +0900

    2022.08.24

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,6 +43,22 @@ public class MemberApiController {
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(),collect);
+    }
+
+    // Entity 를 반환하는것은 요청에 맞게 데이터를 보내기 어렵고
+    // Entity 가 변함에 따라 api 스펙이 바뀌기 때문에 요청별 DTO 를 반환하는것이 좋다.
+
     @Data
     static class CreateMemberResponse {
         private Long id;
@@ -65,4 +83,17 @@ public class MemberApiController {
     static class UpdateMemberRequest {
         private String name;
     }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
 }
