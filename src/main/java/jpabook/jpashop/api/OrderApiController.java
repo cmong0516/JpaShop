e0098bba14c7a37f3a4c1237b3e3354d2dfbf2ae
cmong0516 <cmong0516@gmail.com>
2022-08-26 15:14:46 +0900

    2022.08.26

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.query.OrderFlatDto;
+import jpabook.jpashop.repository.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.query.OrderQueryDto;
 import jpabook.jpashop.repository.query.OrderQueryRepository;
 import lombok.Data;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,14 +39,14 @@ public class OrderApiController {
 
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2() {
-        return orderRepository.findAllByString(new OrderSearch()).stream().map(OrderDto::new).collect(Collectors.toList());
+        return orderRepository.findAllByString(new OrderSearch()).stream().map(OrderDto::new).collect(toList());
         // Order , OrderItem 모두 Dto 로 변환하였다 . 하지만 쿼리가 너무 많이날아가는 N+1 문제가 발생중.
         // join fetch 로 성능최적화를 하자.
     }
 
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
-        return orderRepository.findAllWithItem().stream().map(OrderDto::new).collect(Collectors.toList());
+        return orderRepository.findAllWithItem().stream().map(OrderDto::new).collect(toList());
         // 상품은 2개 인데 order 와 조인하면서 같은 order 가 두개씩 총 네개의 데이터가 나온다.
         // "select distinct o from Order o join fetch o.member m join fetch o.delivery d join fetch o.orderItems oi join fetch oi.item i"
         // distinct 는 DB 에서는 모든 항복이 같아야 중복 제거가 되지만 jpa 에선 id 값으로 판별해서 중복을 제거해준다.
@@ -54,11 +58,11 @@ public class OrderApiController {
 
     @GetMapping("/api/v3.1/orders")
     public List<OrderDto> orderV3_page(
-            @RequestParam(value = "offset",defaultValue = "0") int offset,
-            @RequestParam(value = "limit",defaultValue = "100") int limit
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
     ) {
         //return orderRepository.findAllWithItem().stream().map(OrderDto::new).collect(Collectors.toList());
-        return orderRepository.findAllWithMemberDelivery(offset,limit).stream().map(OrderDto::new).collect(Collectors.toList());
+        return orderRepository.findAllWithMemberDelivery(offset, limit).stream().map(OrderDto::new).collect(toList());
         // n+1 발생.
         // spring.jpa.properties.hibernate.default_batch_fetch_size=100 옵션을 넣어주면
         // n+1 을 1+1 로 최적화 해준다.
@@ -72,6 +76,16 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()), mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList()))).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue())).collect(toList());
+
     }
 
     @Data
@@ -93,7 +107,7 @@ public class OrderApiController {
             address = order.getDelivery().getAddress();
 //            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
 //            orderItems = order.getOrderItems();
-            orderItems = order.getOrderItems().stream().map(OrderItemDto::new).collect(Collectors.toList());
+            orderItems = order.getOrderItems().stream().map(OrderItemDto::new).collect(toList());
         }
     }
 
